f9a0b9d89c61e2f18375d3c2433a4d045bfa7266
cedretaber <cedretaber@gmail.com>
2025-05-26 06:27:02 +0900

    Extract PrimitiveSegment.

--- theories/Main.v
+++ theories/Main.v
@@ -1,60 +1,9 @@
 
 Require Import Stdlib.Reals.Reals.
 Require Import Stdlib.Lists.List.
+Require Import PrimitiveSegment.
 Open Scope R_scope.
 Import ListNotations.
-
-Inductive V : Set :=
-| n : V
-| s : V
-.
-
-Inductive H : Set :=
-| e : H
-| w : H
-.
-
-Inductive C : Set :=
-| cx : C
-| cc : C
-.
-
-Inductive Dir : Set :=
-| Ver : V -> Dir
-| Hor : H -> Dir
-.
-
-Definition i_v (v : V) : V :=
-match v with
-| n => s
-| s => n
-end.
-
-Definition i_h (h : H) : H :=
-match h with
-| e => w
-| w => e
-end.
-
-Definition i (d: Dir) : Dir :=
-match d with
-| Ver v => Ver (i_v v)
-| Hor h => Hor (i_h h)
-end.
-
-Definition i_c (c: C) : C :=
-match c with
-| cx => cc
-| cc => cx
-end. 
-
-(* PrimitiveSegment，単位セグメント *)
-Definition PrimitiveSegment := (V * H * C)%type.
-(* 各要素を取り出す *)
-Definition V_of (x:PrimitiveSegment) := fst(fst x).
-Definition H_of (x:PrimitiveSegment) := snd(fst x).
-Definition C_of (x:PrimitiveSegment) := snd x.
-
 
 (*セグメントは[0,1]->R*Rの関数
 埋め込み関係は次の条件を満たしたもの
