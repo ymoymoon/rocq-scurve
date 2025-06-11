8edcf961ccedaad622e403c850467df3bd974589
cedretaber <cedretaber@gmail.com>
2025-06-11 20:10:37 +0900

    Bump

--- theories/Reduction.v
+++ theories/Reduction.v
@@ -1,6 +1,8 @@
 Require Import Stdlib.Lists.List.
+Require Import ZArith.
 Require Import PrimitiveSegment.
 Import ListNotations.
+Open Scope Z_scope.
 
 (* 向き *)
 Inductive Direction : Set :=
@@ -15,6 +17,24 @@ match x with
 | (s ,w, cx) | (s, e, cc) | (n, e, cc) | (n, w, cx) => Minus
 end.
 
+Definition rotation_difference (ds: list Direction) : Z :=
+  fold_right Z.add 0 (map (fun d =>
+  match d with
+  | Plus => 1
+  | Minus => -1
+  end
+  ) ds).
+
+Lemma rotation_difference_distribution:
+  forall (ds ds': list Direction),
+  rotation_difference (ds ++ ds') = rotation_difference ds + rotation_difference ds'.
+Proof.
+  intros ds ds'.
+  unfold rotation_difference.
+  rewrite map_app.
+  rewrite fold_right_app.
+Admitted.
+
 (* 簡約のルール
  * r1: s1 = s3 = inv(s2) ならば σ1 s1 s2 s3 σ2 を σ1 s1 σ2 に置き換える. +−+ ⇒ +, −+− ⇒ −
  * r2: s1 = s2 = inv(s3) = inv(s4) ならば σ1 s1 s2 s3 s4 σ2 を σ1 s1 s4 σ2 に置き換える. ++−− ⇒ +−, −−++ ⇒ −+
@@ -26,10 +46,28 @@ Inductive Rule : list Direction -> list Direction -> Prop :=
 | R2Minus : Rule [Minus; Minus; Plus; Plus] [Minus; Plus]
 .
 
+Lemma rotation_difference_preservation_rule:
+  forall (ds ds': list Direction), Rule ds ds' -> rotation_difference ds = rotation_difference ds'.
+Proof.
+  intros ds ds' H.
+  destruct H; simpl; try reflexivity.
+Qed.
+
+
 Inductive ReduceDirStep : list Direction -> list Direction -> Prop :=
 | RDS : forall (l r ds ds': list Direction), Rule ds ds' ->
     ReduceDirStep (l ++ ds ++ r) (l ++ ds' ++ r)
 .
+
+Lemma rotation_difference_preservation_step:
+  forall (ds ds': list Direction), ReduceDirStep ds ds' -> rotation_difference ds = rotation_difference ds'.
+Proof.
+  intros ds ds' H.
+  destruct H.
+  apply rotation_difference_preservation_rule in H.
+  repeat rewrite rotation_difference_distribution.
+  apply Z.add_cancel_l. apply Z.add_cancel_r. apply H.
+Qed.
 
 Inductive ReduceDir : list Direction -> list Direction -> Prop :=
 | RDRefl : forall ds, ReduceDir ds ds
@@ -39,3 +77,16 @@ Inductive ReduceDir : list Direction -> list Direction -> Prop :=
 
 Definition Reduce (p p': list PrimitiveSegment): Prop :=
   ReduceDir (map orn p) (map orn p').
+
+Lemma rotation_difference_preservation:
+  forall (ds ds': list Direction), ReduceDir ds ds' -> rotation_difference ds = rotation_difference ds'.
+Proof.
+  intros ds ds' H.
+  induction H as [| ds ds'].
+  - (* RDRefl *)
+    reflexivity.
+  - (* RDTrans *)
+    apply rotation_difference_preservation_step in H.
+    rewrite H.
+    apply IHReduceDir.
+Qed.
