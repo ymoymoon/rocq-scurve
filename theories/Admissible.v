a6f0c06aa605531509e02a8046d5b1438b1fcbc9
y-tak6 <azulwrath13.2@gmail.com>
2025-07-30 20:55:54 +0900

    prove the main theorem

--- theories/Admissible.v
+++ theories/Admissible.v
@@ -4,6 +4,7 @@ From Stdlib Require Import ZArith.
 Require Import PrimitiveSegment.
 Import ListNotations.
 Open Scope Z_scope.
+Require Import Lia.
 
 Definition all_admissibles :=
 [
@@ -28,7 +29,7 @@ Definition all_admissibles :=
   [Plus; Plus; Plus; Minus];
   [Minus; Plus; Plus; Plus];
   [Minus; Minus; Minus; Plus];
-  [Plus; Plus; Plus; Minus];
+  [Plus; Minus; Minus; Minus];
   (* -++- *)
   [Minus; Plus; Plus; Minus];
   [Plus; Minus; Minus; Plus];
@@ -50,15 +51,65 @@ Definition all_admissibles :=
 
 Parameter AdmissibleDirs : list Direction -> Prop.
 
-Definition reduced_form ds reduced:= ReduceDir ds reduced /\ ~ CanReduceDirStep reduced. 
+Axiom notAdmissibleNil : ~ AdmissibleDirs [].
+
+Lemma nil_reduce ds: ReduceDir ds [] -> ds = [].
+Admitted.
+
+Definition reduced ds dsr:= ReduceDir ds dsr /\ ~ CanReduceDirStep dsr. 
 
 Lemma AdmissibleDirs_preserve ds ds': AdmissibleDirs ds -> ReduceDir ds ds' -> AdmissibleDirs ds'.
 Admitted.
 
-Axiom inadmissible_rotation_dif_four: forall ds, rotation_difference ds >= 4 -> ~ AdmissibleDirs ds.
+Axiom inadmissible_rotation_dif_four: forall ds, rotation_difference ds >= 4 \/ rotation_difference ds <= -4 -> ~ AdmissibleDirs ds.
 
-Lemma rotate_dif_four_len_eight ds reduced: reduced_form ds reduced -> (length reduced >= 8)%nat ->rotation_difference ds >= 4.
+Lemma rotate_dif_four_len_eight ds dsr: reduced ds dsr -> (length dsr >= 8)%nat -> rotation_difference ds >= 4 \/ rotation_difference ds <= -4 .
 Admitted.
 
-Theorem reduced_admissible_form : forall ds reduced, AdmissibleDirs ds -> reduced_form ds reduced -> In reduced all_admissibles.
-Admitted.
+Lemma repeat_dif_P n: rotation_difference (repeat Plus n) = Z.of_nat n.
+Admitted. 
+Lemma repeat_dif_M n: rotation_difference (repeat Minus n) = - (Z.of_nat n).
+Admitted. 
+
+Ltac listin := repeat (try (now left);right).
+
+Theorem reduced_admissible_form : forall ds dsr, AdmissibleDirs ds -> reduced ds dsr -> In dsr all_admissibles.
+Proof.
+  intros ds dsr admds [rdc cannotStep].
+  eapply reduced_form in cannotStep as form.
+  destruct (Nat.le_gt_cases 8 (length dsr)) as [ge8 | lt8];
+  [contradict admds; apply inadmissible_rotation_dif_four; eapply rotate_dif_four_len_eight; [split;[exact rdc | exact cannotStep] | exact ge8] |]. 
+  destruct form as [l [m [n [lcond [ncond [PMP | MPM]]]]]].
+  - destruct l as [|[|l]], n as [|[|n]];simpl in PMP; subst.
+    + destruct m as [|[|[|[|m]]]]; try now listin.
+      * contradict admds. simpl in rdc. apply nil_reduce in rdc. subst. exact notAdmissibleNil.
+      * contradict admds. apply inadmissible_rotation_dif_four. rewrite (rotation_difference_preservation _ _ rdc). rewrite rotation_difference_distribution. rewrite repeat_dif_M. simpl. lia.
+    + destruct m as [|[|[|[|[|m]]]]]; try now listin.
+      contradict admds. apply inadmissible_rotation_dif_four. rewrite (rotation_difference_preservation _ _ rdc). change (repeat Minus (S (S (S (S (S m)))))) with ([Minus; Minus; Minus; Minus; Minus] ++ repeat Minus m). repeat rewrite rotation_difference_distribution. rewrite repeat_dif_M. rewrite Z.add_comm. rewrite Z.add_assoc. change (rotation_difference [Plus] + rotation_difference [Minus; Minus; Minus; Minus; Minus]) with (-4). lia.
+    + contradict ncond. lia.
+    + destruct m as [|[|[|[|[|m]]]]]; try now listin.
+      contradict admds. apply inadmissible_rotation_dif_four. rewrite (rotation_difference_preservation _ _ rdc). simpl. rewrite app_nil_r. change (Plus :: Minus :: Minus :: Minus :: Minus :: Minus :: repeat Minus m) with ([Plus; Minus; Minus; Minus; Minus; Minus] ++ repeat Minus m). rewrite rotation_difference_distribution. rewrite repeat_dif_M. change (rotation_difference [Plus;Minus; Minus; Minus; Minus; Minus]) with (-4). lia.
+    + destruct m as [|[|[|[|[|[|m]]]]]]; try now listin.
+      * contradict cannotStep. simpl. exists [Plus]. change ([Plus; Minus; Plus]) with ([] ++ [Plus; Minus; Plus] ++ []). change [Plus] with ([] ++ [Plus] ++ []). now apply RDS.
+      * contradict admds. apply inadmissible_rotation_dif_four. rewrite (rotation_difference_preservation _ _ rdc). simpl. repeat rewrite app_comm_cons. change (Plus :: Minus :: Minus :: Minus :: Minus :: Minus :: Minus :: repeat Minus m) with ([Plus; Minus; Minus; Minus; Minus; Minus; Minus] ++ repeat Minus m). repeat rewrite rotation_difference_distribution. rewrite repeat_dif_M. rewrite Z.add_shuffle0. change (rotation_difference [Plus; Minus; Minus; Minus; Minus; Minus; Minus] + rotation_difference [Plus]) with (-4). lia.
+    + contradict lcond. lia.
+    + contradict lcond. lia.
+    + contradict lcond. lia.
+    + contradict lcond. lia.
+  - destruct l as [|[|l]], n as [|[|n]];simpl in MPM; subst.
+    + destruct m as [|[|[|[|m]]]]; try now listin.
+      * contradict admds. simpl in rdc. apply nil_reduce in rdc. subst. exact notAdmissibleNil.
+      * contradict admds. apply inadmissible_rotation_dif_four. rewrite (rotation_difference_preservation _ _ rdc). rewrite rotation_difference_distribution. rewrite repeat_dif_P. simpl. lia.
+    + destruct m as [|[|[|[|[|m]]]]]; try now listin.
+      contradict admds. apply inadmissible_rotation_dif_four. rewrite (rotation_difference_preservation _ _ rdc). change (repeat Plus (S (S (S (S (S m)))))) with ([Plus; Plus; Plus; Plus; Plus] ++ repeat Plus m). repeat rewrite rotation_difference_distribution. rewrite repeat_dif_P. rewrite Z.add_comm. rewrite Z.add_assoc. change (rotation_difference [Minus] + rotation_difference [Plus; Plus; Plus; Plus; Plus]) with 4. lia.
+    + contradict ncond. lia.
+    + destruct m as [|[|[|[|[|m]]]]]; try now listin.
+      contradict admds. apply inadmissible_rotation_dif_four. rewrite (rotation_difference_preservation _ _ rdc). simpl. rewrite app_nil_r. change (Minus :: Plus :: Plus :: Plus :: Plus :: Plus :: repeat Plus m) with ([Minus; Plus; Plus; Plus; Plus; Plus] ++ repeat Plus m). rewrite rotation_difference_distribution. rewrite repeat_dif_P. change (rotation_difference [Minus; Plus; Plus; Plus; Plus; Plus]) with 4. lia.
+    + destruct m as [|[|[|[|[|[|m]]]]]]; try now listin.
+      * contradict cannotStep. simpl. exists [Minus]. change ([Minus; Plus; Minus]) with ([] ++ [Minus; Plus; Minus] ++ []). change [Minus] with ([] ++ [Minus] ++ []). now apply RDS.
+      * contradict admds. apply inadmissible_rotation_dif_four. rewrite (rotation_difference_preservation _ _ rdc). simpl. repeat rewrite app_comm_cons. change (Minus :: Plus :: Plus :: Plus :: Plus :: Plus :: Plus :: repeat Plus m) with ([Minus; Plus; Plus; Plus; Plus; Plus; Plus] ++ repeat Plus m). repeat rewrite rotation_difference_distribution. rewrite repeat_dif_P. rewrite Z.add_shuffle0. change (rotation_difference [Minus; Plus; Plus; Plus; Plus; Plus; Plus] + rotation_difference [Minus]) with 4. lia.
+    + contradict lcond. lia.
+    + contradict lcond. lia.
+    + contradict lcond. lia.
+    + contradict lcond. lia.
+Qed.
