3ed6f208a022b634ff8030efbe9250701d3e469f
ymoymoon <148025734+ymoymoon@users.noreply.github.com>
2025-12-04 19:31:18 +0900

    make Lemma in AdmissibilityPreservation (Prop3 <-)

--- theories/AdmissibilityPreservation.v
+++ theories/AdmissibilityPreservation.v
@@ -1,7 +1,6 @@
 Require Import Admissible.
 Require Import Reduction.
 Require Import Stdlib.Reals.Reals.
-Require Import ListExt.
 Require Import Embed.
 Require Import PrimitiveSegment.
 Require Import Segment.
@@ -10,7 +9,7 @@ Import ListNotations.
 (* 単方向 scurve *)
 Parameter one_way_scurve : list Direction -> Prop.
 
-(* scurve に直してから埋め込む． *)
+(* scurve に直してから埋め込む．そうしないなら，連結していない Seg 列を省けるように． *)
 Parameter embed_listDir : list Direction -> list Segment -> Prop.
 
 (* TODO: 空リストは含まない．含めるなら，後の定理の必要な部分に not nil 制約を入れる *)
@@ -25,10 +24,32 @@ Definition in_rect ls rr := rectangular_region ls rr.
 
 (* 疎：単方向曲線 sub_ls の周囲に全然関係ないセグメントが侵入してこない *)
 Definition sparse (sub_ls : one_way_embedding) (ls : list Segment) : Prop :=
-	sublist (proj1_sig sub_ls) ls
+	exists l r, ls = l ++ (proj1_sig sub_ls) ++ r
 	/\ forall rr, 
 		(exists seg, onExtendSegment ls seg rr) -> ~ in_rect sub_ls rr.
+		(* /\ (l = [] -> (* sub_ls が端点を含む場合，延長線上にも関係ないセグメントが近づかないことを要請 *)
+				let X1 := head_seg sub_ls default_segment in 
+				let width := (term_x X1) - (init_x X1) in (* 矩形の横幅 *)
+				let length := (term_y X1) - (init_y X1) in (* 矩形の縦幅 *)
+				forall p, onHeadSegment X1 p -> 
+					let rect := rectangular_from_diagonal ((fst p)-width, (snd p)-length) p in (* 矩形 *)
+					forall q, 
+					(exists seg, onExtendSegment ls seg q /\ ~ onHeadSegment X1 q) 
+					-> ~ rect q) *)
 
+
+Lemma P_is_oneway : one_way_scurve [Plus].
+Proof. (* ここは one_way_scurve の定義さえできれば示せる？ *)
+Admitted.
+
+Lemma embedding_P_is_oneway : forall seg, 
+	embed_listDir [Plus] [seg] -> is_one_way_embedding [seg].
+Proof.
+Admitted.
+
+Lemma PMP_is_oneway : one_way_scurve [Plus; Minus; Plus].
+Proof.
+Admitted.
 
 (* 許容可能ならば，疎な開埋め込みが存在する *)
 Lemma embed_sparsely (dl dr : list Direction) (sub_ds : list Direction) :
@@ -60,26 +81,43 @@ Lemma seg_in_rectangle_keep_openness : forall sub_ls l ls r rs seg,
 	-> ~ close ((l :: ls) ++ [seg] ++ (r :: rs)).
 Proof. Admitted.
 
-(* [+-+ => +] での簡約で許容可能性が保存される *)
+
+(* [+-+ => +] での簡約で，簡約元が許容可能なら簡約先も許容可能 *)
 Lemma AdmissibleDirs_r1_Plus: forall l r,
   AdmissibleDirs (l ++ [Plus; Minus; Plus] ++ r) -> AdmissibleDirs (l ++ [Plus] ++ r).
 Proof.
-	remember [Plus; Minus; Plus] as PMP eqn: HeqPMP.
 	intros l r admds. destruct l as [ | l'].
 	- (* 端点を含む簡約 *) admit.
 	- destruct r as [ | r'].
 		+ (* 端点を含む簡約 *) admit.
 		+ (* 簡約にかかわった部分の両端に別のセグメントが存在する場合 *)
-			assert (Honeway : one_way_scurve PMP). {
-				admit.
-			}
-			apply (embed_sparsely _ _ _ admds) in Honeway as 
-				[ll [lr [sub_ls [Hl [Hr [HPMP [Hopen Hsparse]]]]]]].
 			unfold AdmissibleDirs. intros ps Hps. unfold admissible.
+			pose proof PMP_is_oneway as PMP_is_oneway.
+			apply (embed_sparsely _ _ _ admds) in PMP_is_oneway as 
+				[ll [lr [sub_ls [Hl [Hr [HPMP [Hopen Hsparse]]]]]]].
 			destruct (embed_in_rectangle sub_ls) as 
 				[seg [Hin_rect [Hinit Hterm]]].
 			exists (ll ++ [seg] ++ lr). split.
 			* (* 簡約後の向き列の埋め込みになっていること *) admit.
 			* (* その埋め込みが開であること *) admit.
 			(* apply seg_in_rectangle_keep_openness. *)
+Admitted.
+
+(* Plus (の向きを持つ Primitive Segment)の埋め込みを，[Plus; Minus; Plus] の埋め込みとなる３つに矩形内で分割できる
+	もっと一般的にしたい *)
+Lemma P_to_PMP : forall seg : Segment,
+	embed_listDir [Plus] [seg]
+	-> exists seg1 seg2 seg3,
+		embed_listDir [Plus; Minus; Plus] [seg1; seg2; seg3] (* この内部で seg1-3 が連結していることは示されてほしい *)
+		(* /\ forall rr, (exists s:Segment, onExtendSegment [seg1; seg2; seg3] s rr 
+			-> in_rect (exist _ _ [seg] embedding_P_is_oneway) rr) (* 矩形の内部で分割できている *) *)
+		/\ init seg1 = init seg
+		/\ term seg3 = term seg.
+
+(* [+-+ => +] での簡約で，簡約先が許容可能ならもともと許容可能 *)
+Lemma AdmissibleDirs_r1_Plus_inv: forall l r,
+  AdmissibleDirs (l ++ [Plus] ++ r) -> AdmissibleDirs (l ++ [Plus; Minus; Plus] ++ r).
+Proof.
+	intros l r admds. unfold AdmissibleDirs. intros ps Hps. unfold admissible. 
+	(* apply seg_in_rectangle_keep_openness. *)
 Admitted.
