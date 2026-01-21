7cc5f05d891de06c849e612f4bc30cbb4cf706b1
ymoymoon <148025734+ymoymoon@users.noreply.github.com>
2026-01-21 09:45:41 +0900

    contradiction in Segment.v

--- theories/AdmissibilityPreservation.v
+++ theories/AdmissibilityPreservation.v
@@ -7,6 +7,12 @@ Require Import Segment.
 Import ListNotations.
 
 
+Lemma F : False.
+Proof.
+	apply (neq_init_term_x (fun r => (1, 1))).
+	reflexivity.
+Qed.
+
 (* 単なるリストに関する補題 *)
 
 Lemma list_head_tail : forall {A} (dummy: A) (l: list A),
